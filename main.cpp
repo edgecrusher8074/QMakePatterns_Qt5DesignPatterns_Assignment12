77a1800b4d87e414ad80c2756fca95a99e1689f1
Hinteregger, Thomas <58877949+edgecrusher8074@users.noreply.github.com>
2021-09-16 07:28:37 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <QCoreApplication>
+
+/**
+ * @brief Assignment 10 for Qt5 Design Patterns Tutorial on Udemy.
+ * @details Use the pre- and postlink option to run external scripts.
+ * @author edgecrusher8074
+ */
+
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    return a.exec();
+}
